23a0891c2c9c0cbe00f43306a64ba31aef11982b
S.M. Khalid Mahmud <khalidccnu@gmail.com>
2024-02-12 21:31:34 +0600

    imagekit added for upload provider

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,16 @@
-export default () => ({});
+export default ({ env }) => ({
+  upload: {
+    config: {
+      provider: "strapi-provider-upload-imagekit",
+      providerOptions: {
+        publicKey: env("IK_PUBLIC_KEY"),
+        privateKey: env("IK_PRIVATE_KEY"),
+        urlEndpoint: env("IK_ID"),
+
+        params: {
+          folder: "/digitree",
+        },
+      },
+    },
+  },
+});
